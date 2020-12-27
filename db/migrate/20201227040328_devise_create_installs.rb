24b5edb4e870fac3e9307b604d283d31b8061220
hamada456 <n87.takaya.hamada@gmail.com>
2020-12-27 13:10:37 +0900

    usersテーブルにカラム追加

--- db/migrate/20201227040328_devise_create_installs.rb
+++ db/migrate/20201227040328_devise_create_installs.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-class DeviseCreateUsers < ActiveRecord::Migration[6.0]
+class DeviseCreateInstalls < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
-      t.string :name,               null: false
+    create_table :installs do |t|
+
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -36,9 +36,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :installs, :email,                unique: true
+    add_index :installs, :reset_password_token, unique: true
+    # add_index :installs, :confirmation_token,   unique: true
+    # add_index :installs, :unlock_token,         unique: true
   end
 end
