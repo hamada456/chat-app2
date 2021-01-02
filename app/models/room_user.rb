dc7d6673a41c1033ff9fe8d22c47aa69ae59038c
hamada456 <n87.takaya.hamada@gmail.com>
2021-01-02 15:28:15 +0900

    res

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,7 +1,4 @@
 class RoomUser < ApplicationRecord
-<<<<<<< Updated upstream
   belongs_to :room
   belongs_to :user
-=======
->>>>>>> Stashed changes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-<<<<<<< Updated upstream
          
-=======
->>>>>>> Stashed changes
   has_many :room_users
   has_many :rooms, through: :room_users
 end
