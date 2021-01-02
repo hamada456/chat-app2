6b10cc3bb2f7d1b92741b0c41a3e167a1d226ea1
hamada456 <n87.takaya.hamada@gmail.com>
2021-01-02 18:40:07 +0900

    部屋の作成の保存

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,22 @@
 class RoomsController < ApplicationController
+
   def new
     @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # 配列の保存を許可するストロングパラメーター（配列に対して保存を許可したい場合はキーに対し[]を値として記述）
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
   end
 end
